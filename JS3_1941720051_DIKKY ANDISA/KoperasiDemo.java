17d60ae755d4e7789f50b71430baacbdc0e579f8
dikkyandisa <71000593+dikkyandisa@users.noreply.github.com>
2020-09-16 21:54:56 +0700

    JS3-DIKKY ANDISA

--- JS3_1941720051_DIKKY ANDISA/KoperasiDemo.java
+++ JS3_1941720051_DIKKY ANDISA/KoperasiDemo.java
@@ -1,19 +1,58 @@
-/**
- * KoperasiDemo
- */
 public class KoperasiDemo {
-
-    public static void main(String[] args) {
-        Anggota anggota1 = new Anggota("Iwan","Jalan Mawar");
-        System.out.println("Simpan" + anggota1.getNama()+" : Rp "+ anggota1.getSimpanan());
-        
-        anggota1.setNama("Iwan Setiawan");
-        anggota1.setAlamat("jalan sukarno hatta no 10");
-        anggota.setor(100000);
-        System.out.println("Simpan" + anggota1.getNama()+" : Rp "+ anggota1.getSimpanan());
-
-        anggota1.pinjam(5000);
-        System.out.println("Simpan" + anggota1.getNama()+" : Rp "+ anggota1.getSimpanan());
-    }
-}
+    private int noKTP ;
+    private String nama;
+    private int limit=5000000;
+    private int jumlahPinjam;
     
+    
+    KoperasiDemo(int noKTP, String nama, int limit) {
+        this.nama = nama;
+        this.noKTP = noKTP;
+    
+    } 
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    public void setRekening(int rekening) {
+        this.noKTP = noKTP;
+    }
+    public String getNama() {
+        return nama;
+    }
+    public int getNoKTP() {
+        return noKTP;
+    }
+    public void setLimitPinjam(int limit){
+        this.limit=limit;
+    }
+
+    public int getLimitPinjam() {
+      return limit;
+            
+    }
+    public int getJumlahPinjaman(){
+          return jumlahPinjam;
+    }
+    public int pinjam(int pinjam){
+       if(pinjam <= limit){
+            jumlahPinjam += pinjam;  
+        }
+        else{
+              System.out.println("Maaf, jumlah pinjaman limit");
+        }
+        return jumlahPinjam;
+    }
+    public int angsur(int angsur){
+        jumlahPinjam -= angsur;
+        if( angsur>(0.01*jumlahPinjam)){
+            System.out.println("Angsuran anda diterima");
+        }
+        else{
+            System.out.println("Maaf angsuran harus 10% dari jumlah pinjaman");
+        }
+        return jumlahPinjam;
+    
+   
+    }
+
+}
